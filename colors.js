1043917038069a22c7eca13319b119226d656b70
0xjav <whuzz@protonmail.com>
2021-02-03 12:27:02 -0300

    background colors updated for gradients

--- colors.js
+++ colors.js
@@ -2,16 +2,17 @@ $("#search-input").keyup(function(e) {
   abc = document.getElementById("search-input").value;
 
   if (abc.match(/youtube/g)) {
-    $("body").css("background-color", "rgb(205, 32, 31)");
+    $("body").css("background-image", "linear-gradient(to right, rgb(205, 32, 31), rgb(112, 16, 16))");
   } else if (abc.match(/twitter/g)) {
-    $("body").css("background-color", "rgb(27, 122, 183)");
+    $("body").css("background-image", "linear-gradient(to right, rgb(27, 122, 183), rgb(11, 54, 80))");
   } else if (abc.match(/reddit/g)) {
-    $("body").css("background-color", "rgb(255, 70, 2)");
+    $("body").css("background-image", "linear-gradient(to right, rgb(255, 70, 2), rgb(156, 42, 0))");
   } else if (abc.match(/twitch/g)) {
-    $("body").css("background-color", "rgb(88, 60, 145)");
+    $("body").css("background-image", "linear-gradient(to right, rgb(88, 60, 145), rgb(54, 37, 88))");
   } else if (abc.match(/github/g)) {
-    $("body").css("background-color", "rgb(36, 41, 46)");
+    $("body").css("background-image", "linear-gradient(to right, rgb(36, 41, 46), rgb(15, 17, 19)");
   } else {
     $("body").css("background-color", "rgb(14, 14, 14)");
+    $("body").css("background-image", "none");
   }
 })
