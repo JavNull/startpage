720635cd0733b34919076eba4ea40be988aadac9
0xjav <whuzz@protonmail.com>
2021-02-03 12:26:32 -0300

    added new regex function for ip adresses

--- form.js
+++ form.js
@@ -2,7 +2,7 @@ $('#search-input').keyup(function(){
   if($(this).val() == ''){
     $('#search-input').hide();
     $('#clock').show();
-
+    $("body").css("background-color", "rgb(14, 14, 14)");
   }
   
 });
@@ -14,9 +14,12 @@ $(document).ready(function(){
     if(e.which == 13){
       e.preventDefault();
       abc = document.getElementById('search-input').value;
-      if (abc.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)) {
-        // It means URL
+      if (abc.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)){
+        // for urls
         window.open(`https://${abc}`, "_self", false);
+    } else if (abc.match(/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/g)) {
+        // for ip addresses
+        window.open(`http://${abc}`, "_self", false);
     } else {
         window.open(`https://google.com/search?as_q=${abc}`, "_self", false);
       }
