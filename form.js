4b3cd422a39c91b45266644ce900324d63d2c911
JavNull <whuzz@protonmail.com>
2021-02-24 18:28:52 -0300

    added support for urls with http/https on input

--- form.js
+++ form.js
@@ -14,14 +14,17 @@ $(document).ready(function(){
     if(e.which == 13){
       e.preventDefault();
       abc = document.getElementById('search-input').value;
-      if (abc.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)){
-        // for urls
-        window.open(`https://${abc}`, "_self", false);
-    } else if (abc.match(/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/g)) {
+      if (abc.match(/(http|https)?:\/\//g)){
+        // for urls with http/https
+        window.open(`${abc}`, "_self", false);
+      } else if (abc.match(/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/g)) {
         // for ip addresses
-        window.open(`http://${abc}`, "_self", false);
-    } else {
-      window.open(`https://google.com/search?as_q=${abc}`, "_self", false);
+          window.open(`http://${abc}`, "_self", false);
+      } else if (abc.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)) {
+          // for urls without http or https
+          window.open(`http://${abc}`, "_self", false);
+      } else {
+          window.open(`https://google.com/search?as_q=${abc}`, "_self", false);
       }
     }
   });
