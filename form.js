92a2d31299accea8000fb6fc0243d9876bfe135b
JavNull <whuzz@protonmail.com>
2021-03-16 14:20:48 -0300

    added y/ prefix for youtube search

--- form.js
+++ form.js
@@ -17,14 +17,23 @@ $(document).ready(function(){
       if (abc.match(/(http|https)?:\/\//g)){
         // for urls with http/https
         window.open(`${abc}`, "_self", false);
+
       } else if (abc.match(/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/g)) {
         // for ip addresses
-          window.open(`http://www.${abc}`, "_self", false);
+        window.open(`http://www.${abc}`, "_self", false);
+
       } else if (abc.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)) {
         // for urls without http://www or https://www
-          window.open(`http://www.${abc}`, "_self", false);
+        window.open(`http://www.${abc}`, "_self", false);
+
+      } else if (abc.match(/y\/.+/g)) {
+        // y/ prefix
+        abc_without_prefix = abc.replace(/y\//g, '')
+        window.open(`https://www.youtube.com/results?search_query=${abc_without_prefix.replace(/ /g, '+')}`, "_self", false);
+
       } else {
         window.open(`https://google.com/search?as_q=${abc}`, "_self", false);
+
       }
     }
   });
