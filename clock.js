08b6261144ac74cd4fd6ce6426052cb1556af3fe
0xjav <whuzz@protonmail.com>
2021-06-07 17:41:44 -0300

    updated code to be more readble

--- clock.js
+++ clock.js
@@ -4,12 +4,15 @@ function time() {
   var d = new Date();
   var m = d.getMinutes();
   var h = d.getHours();
-  if (h > 12) h -= 12;
   if (h < 10) {h = "0" + h};
 
   if (m < 10) {m = "0" + m};
 
-  clock.textContent = h + " " + m;
+  if (h > 12) {
+    clock.textContent = `${h - 12} ${m}`;
+  } else {
+    clock.textContent = `${h} ${m}`;
+  }
 }
 
 setInterval(time, 5)
