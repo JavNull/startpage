22cf882e56d04b2d4bd933e3b8d79e36686a10df
pngnull <whuzz@protonmail.com>
2021-06-20 13:08:52 -0300

    updated to 24 format

--- clock.js
+++ clock.js
@@ -4,17 +4,10 @@ function time() {
   var d = new Date();
   var m = d.getMinutes();
   var h = d.getHours();
-  if (h < 10) {h = "0" + h};
 
   if (m < 10) {m = "0" + m};
 
-  if (h > 12) {
-    clock.innerHTML =  `${h - 12} ${m} <span id="pm">pm</span>`
-  } else if (h = 12) {
-      clock.innerHTML =  `${h} ${m} <span id="pm">pm</span>`
-  } else {
-    clock.innerHTML =  `${h} ${m} <span id="am">am</span>`
-  }
+  clock.innerHTML =  `${h} ${m}`
 }
 
 setInterval(time, 5)
